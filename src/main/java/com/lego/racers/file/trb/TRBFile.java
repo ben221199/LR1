9593170b6f720eed28b59161cc1cb674a16a94ed
Ben <ben@yocto.nu>
2022-08-25 20:37:54 +0200

    Add serialization for TRB

--- src/main/java/com/lego/racers/file/trb/TRBFile.java
+++ src/main/java/com/lego/racers/file/trb/TRBFile.java
@@ -1,5 +1,6 @@
 package com.lego.racers.file.trb;
 
+import com.lego.racers.binary.BinaryArray;
 import com.lego.racers.binary.BinaryFile;
 import com.lego.racers.binary.BinaryFloat;
 import com.lego.racers.binary.BinaryIntegerSigned;
@@ -7,13 +8,14 @@ import com.lego.racers.binary.BinaryObject;
 import com.lego.racers.binary.BinaryProperty;
 import com.lego.racers.binary.BinaryString;
 import com.lego.racers.binary.BinaryStruct;
+import com.lego.racers.binary.BinaryToken;
 import com.lego.racers.binary.object.Position;
 import com.lego.racers.file.trb.object.Trigger;
 import com.lego.racers.file.trb.object.Triggers;
 
-public class TRBFile{
+import java.util.List;
 
-	public static final byte PROPERTY_TRIGGERS = 0x27;
+public class TRBFile{
 
 	public static final byte PROPERTY_TRIGGER = 0x27;
 	public static final byte PROPERTY_x29 = 0x29;
@@ -26,15 +28,81 @@ public class TRBFile{
 	private Triggers triggers;
 
 	public byte[] toBytes(){
-		//TODO
-		return new byte[0];
+		BinaryFile bin = new BinaryFile();
+		if(this.triggers!=null){
+			BinaryProperty triggersProp = new BinaryProperty();
+			triggersProp.setKey(new BinaryToken(TRBFile.PROPERTY_TRIGGER));
+			BinaryArray arr = new BinaryArray();
+			arr.getTokens().add(new BinaryIntegerSigned(this.triggers.size()));
+			BinaryObject obj = new BinaryObject();
+			this.toBytesTriggers(this.triggers,obj);
+			triggersProp.setValues(List.of(arr,obj));
+			bin.addProperty(triggersProp);
+		}
+		return bin.toBytes();
+	}
+
+	private void toBytesTriggers(Triggers triggers,BinaryObject obj){
+		for(Trigger trigger : triggers){
+			BinaryProperty triggerProp = new BinaryProperty();
+			triggerProp.setKey(new BinaryToken(TRBFile.PROPERTY_TRIGGER));
+			BinaryObject subObj = new BinaryObject();
+			this.toBytesTrigger(trigger,subObj);
+			triggerProp.setValues(List.of(subObj));
+			obj.addProperty(triggerProp);
+		}
+	}
+
+	private void toBytesTrigger(Trigger trigger,BinaryObject obj){
+		if(trigger.getX29()!=null){
+			BinaryProperty seatProp = new BinaryProperty();
+			seatProp.setKey(new BinaryToken(TRBFile.PROPERTY_x29));
+			BinaryFloat f1 = new BinaryFloat(trigger.getX29().getX());
+			BinaryFloat f2 = new BinaryFloat(trigger.getX29().getY());
+			BinaryFloat f3 = new BinaryFloat(trigger.getX29().getZ());
+			seatProp.setValues(List.of(f1,f2,f3));
+			obj.addProperty(seatProp);
+		}
+		if(trigger.getX2A()!=null){
+			BinaryProperty x2AProp = new BinaryProperty();
+			x2AProp.setKey(new BinaryToken(TRBFile.PROPERTY_x2A));
+			BinaryFloat f1 = new BinaryFloat(trigger.getX2A());
+			x2AProp.setValues(List.of(f1));
+			obj.addProperty(x2AProp);
+		}
+		if(trigger.getX2B()!=null){
+			BinaryProperty x2BProp = new BinaryProperty();
+			x2BProp.setKey(new BinaryToken(TRBFile.PROPERTY_x2B));
+			BinaryIntegerSigned f1 = new BinaryIntegerSigned(trigger.getX2B());
+			x2BProp.setValues(List.of(f1));
+			obj.addProperty(x2BProp);
+		}
+		if(trigger.getX2D()!=null){
+			BinaryProperty x2DProp = new BinaryProperty();
+			x2DProp.setKey(new BinaryToken(TRBFile.PROPERTY_x2D));
+			BinaryString f1 = new BinaryString(trigger.getX2D());
+			x2DProp.setValues(List.of(f1));
+			obj.addProperty(x2DProp);
+		}
+		if(trigger.getX2E()!=null){
+			BinaryProperty x2EProp = new BinaryProperty();
+			x2EProp.setKey(new BinaryToken(TRBFile.PROPERTY_x2E));
+			BinaryIntegerSigned f1 = new BinaryIntegerSigned(trigger.getX2E());
+			x2EProp.setValues(List.of(f1));
+			obj.addProperty(x2EProp);
+		}
+		if(trigger.getX2F()){
+			BinaryProperty x2FProp = new BinaryProperty();
+			x2FProp.setKey(new BinaryToken(TRBFile.PROPERTY_x2F));
+			obj.addProperty(x2FProp);
+		}
 	}
 
 	public static TRBFile from(byte[] bytes){
 		BinaryFile bin = BinaryFile.from(bytes);
 		TRBFile trb = new TRBFile();
 		for(BinaryProperty prop : bin.getProperties()){
-			if(prop.getKey().getToken()==TRBFile.PROPERTY_TRIGGERS){
+			if(prop.getKey().getToken()==TRBFile.PROPERTY_TRIGGER){
 				TRBFile.fromTriggers(trb,(BinaryObject) prop.getValues().get(1));
 				continue;
 			}
